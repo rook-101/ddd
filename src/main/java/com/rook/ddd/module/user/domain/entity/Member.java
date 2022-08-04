f047b12c07f80d7b5e93b979a7abe200707ba3f2
Rook(룩) <rook@101.inc>
2022-08-04 20:13:18 +0900

    feat: Add unique for username

--- src/main/java/com/rook/ddd/module/user/domain/entity/Member.java
+++ src/main/java/com/rook/ddd/module/user/domain/entity/Member.java
@@ -25,7 +25,7 @@ public class Member {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
     @Column(name = "password", nullable = false)
